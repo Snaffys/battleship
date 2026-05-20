4ba70366554e3bc07df780fc9dc51bebe469d148
Dan <grezindanil@gmail.com>
2026-05-20 14:15:20 +0300

    Move hw3-specific files out of hw2 and changed Main so it doesn't use these files

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,12 +1,7 @@
 package app
 
 import console.runBattleshipConsole
-import gui.runBattleshipGui
 
-fun main(args: Array<String>) {
-    if (args.contains("--console")) {
-        runBattleshipConsole()
-    } else {
-        runBattleshipGui()
-    }
+fun main() {
+    runBattleshipConsole()
 }
